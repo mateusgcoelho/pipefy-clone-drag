6711ce4ce2022fead53ab33cf261bff4fe0b7041
João Mateus Games Coelho <mateusgamescoelho547@gmail.com>
2020-08-01 12:41:14 -0300

    Primeiro upload da aplicação

---
+++ src/components/List/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { MdAdd } from 'react-icons/md';
+
+import Card from '../Card';
+
+import { Container } from './styles';
+
+function List({ data, index: listIndex }) {
+  return (
+      <Container done={data.done}>
+        <header>
+          <h2>{data.title}</h2>
+
+          {data.creatable && (
+            <button type='button'>
+              <MdAdd size={24} color="#FFF" />
+            </button>
+          )}
+        </header>
+
+        <ul>
+          { data.cards.map((card, index) => <Card index={index} listIndex={listIndex} key={card.id} data={card} />) }
+        </ul>
+      </Container>
+  );
+}
+
+export default List;
